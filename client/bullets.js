5e52afc4252dd7522684c5dae14fe3f7764f61ab
svwielga4@gmail.com <svwielga4@gmail.com>
2017-02-10 14:27:39 -0800

    still playing with bullets

--- client/bullets.js
+++ client/bullets.js
@@ -3,8 +3,6 @@ function Bullet(parent){
     this.parent = parent;
     this.domElem = null;
 
-    this.x = parent.x;
-    this.y = parent.y;
     this.speed = 50;
     this.trajectory = parent.rotation;
 
@@ -13,29 +11,29 @@ function Bullet(parent){
             id: this.id,
             class: 'bullet',
             css: {
-                left: self.x,
-                top: self.y,
+                left: parent.x,
+                top: parent.y,
                 'transition-duration': 0.1 + 's',
-                height:1 + 'px',
-                width:1 + 'px'
+                height: 1 + 'px',
+                width: 1 + 'px'
 
             }
         });
         return this.domElem;
     };
 
-    this.shootBullet = function () {
-        var temp_angle = self.trajectory + 280;
-        var delta_x = Math.cos(self.get_radians(temp_angle)) * self.speed;
-        var delta_y = Math.sin(self.get_radians(temp_angle)) * self.speed;
-        console.log()
-        // self.x += delta_x;
-        // self.y += delta_y;
-        self.domElem.css({
-            top : '+='+delta_y+'px',
-            left : '+='+delta_x+'px'
-        })
-    }
+    // this.shootBullet = function () {
+    //     var temp_angle = self.trajectory + 280;
+    //     var delta_x = Math.cos(self.get_radians(temp_angle)) * self.speed;
+    //     var delta_y = Math.sin(self.get_radians(temp_angle)) * self.speed;
+    //     console.log()
+    //     // self.x += delta_x;
+    //     // self.y += delta_y;
+    //     self.domElem.css({
+    //         top : '+='+delta_y+'px',
+    //         left : '+='+delta_x+'px'
+    //     })
+    // }
 
     this.get_radians = function (degrees) {
         return (Math.PI / 180) * degrees;
