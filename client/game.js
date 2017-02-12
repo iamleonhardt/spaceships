a713c885eaf1e8ef0b5633ddc17f92ee93f45cb1
Bill Leonhardt <iamleonhardt@gmail.com>
2017-02-12 14:40:13 -0800

    everything in place for teamSelectMenu, just need to make ship from it now

--- client/game.js
+++ client/game.js
@@ -2,36 +2,63 @@ function GameController(socket) {
     var self = this;
     this.shipList = {};
     this.mapController = null;
+    this.selectedTeam = '';
+    this.selectedShip = '';
 
     this.init = function (gameAreaSelector) {
         this.gameArea = $(gameAreaSelector);
         this.instantiateMap(this.handleMapLoad.bind(this));
-        this.addTeamClickHandlers();
+        this.addTeamSelectMenuClickHandlers();
+
+    };
+
+    this.addTeamSelectMenuClickHandlers = function(){
+        $('#Red').click(self.teamSelected);
+        $('#Green').click(self.teamSelected);
+        $('#Gold').click(self.teamSelected);
+
         $('.shipChoice').click(this.shipSelected);
 
+        $('#joinBtn').click(self.joinTheFightBtn);
     };
 
-    this.addTeamClickHandlers = function(){
-        $('#Red').click(self.showTeamShips);
-        $('#Green').click(self.showTeamShips);
-        $('#Gold').click(self.showTeamShips);
-    };
-
-    this.showTeamShips = function (){
+    this.teamSelected = function () {
         var color = $(this).attr('id');
         console.log('team was clicked and this is : ', this);
-        $('#selectShipText').text('Select a Ship');
-
-        for (var i = 1; i < 8; i++){
-            $('#shipChoice' + i).attr('class', 'shipChoice ship' + color + i);
+        // Store team choice
+        self.selectedTeam = $(this).attr('id');
+        $('#menuInstructions').text('Select a Ship');
+        // Show ships for team and add data-value
+        for (var i = 1; i < 8; i++) {
+            $('#shipChoice' + i).attr({
+                'data-ship-sprite': 'ship' + color + i,
+                class: 'shipChoice ship' + color + i
+            });
         }
     };
 
     this.shipSelected = function(){
-      //store value
+      //store ship choice
+        self.selectedShip = this.dataset.shipSprite;
+        console.log('data is : ', this.dataset.shipSprite);
         console.log('click and this is : ', this);
         $('.shipChoice').removeClass('selectedShip');
         $(this).addClass('selectedShip');
+    };
+
+    this.joinTheFightBtn = function(){
+        var pilotName = $('#pilotNameInput').val();
+        if (self.selectedTeam == ''){
+            $('#menuInstructions').text('Please Select a Team First');
+        }else if (self.selectedShip == ''){
+            $('#menuInstructions').text('Please Select a Ship First');
+
+        }else{
+            // Make ship with (self.selectedTeam, self.selectedShip, pilotName)
+            if(pilotName == ''){
+                pilotName = 'Drone';
+            }
+        }
     };
 
     this.instantiateMap = function (mapLoadedHandler, options) {
